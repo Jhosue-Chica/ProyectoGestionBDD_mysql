f06d8ad4b02ef3f252ac652b792aa8b7dedbde97
Jhosue-Chica <jichica@espe.edu.ec>
2024-01-25 20:20:32 -0500

    Update connection message

--- AppProyectoBDD/src/Conexion.java
+++ AppProyectoBDD/src/Conexion.java
@@ -10,7 +10,7 @@ public class Conexion {
     public static void main(String[] args) {
         obtenerConexion();
         //SwingUtilities.invokeLater(InterfazEmpleado::new);
-        System.out.println("Conexion activada: ");
+        System.out.println("Conexion activada si: ");
     }
 
     public static Connection obtenerConexion() {
