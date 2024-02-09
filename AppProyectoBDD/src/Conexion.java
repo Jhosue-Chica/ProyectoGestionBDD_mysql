f12d99b84e9e9d631c351239c58297c7921484b9
NightCrawler927 <nelsonbarahona2011@gmail.com>
2024-02-08 19:20:07 -0500

    Update Conexion

--- AppProyectoBDD/src/Conexion.java
+++ AppProyectoBDD/src/Conexion.java
@@ -1,22 +1,50 @@
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
     private static final String URL = "jdbc:mysql://10.41.1.128:23306/ProyectoU1";
-    private static final String USER = "root";
-    private static final String PASSWORD = "admin";
+    private static final String USER_DBA = "root"; // DBA
+    private static final String USER_FUNCIONARIO = "funcionario"; 
+    private static final String USER_REPORTES = "reportes";
+    private static final String PASSWORD_DBA = "270902"; // 12345
+    private static final String PASSWORD_FUNCIONARIO = "1234";
+    private static final String PASSWORD_REPORTES = "123";
 
     public static void main(String[] args) {
         obtenerConexion();
         //SwingUtilities.invokeLater(InterfazEmpleado::new);
         System.out.println("Conexion activada: ");
     }
-
+    // Conexion usuario DBA
     public static Connection obtenerConexion() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver"); // Cargar el controlador de MySQL
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+            return DriverManager.getConnection(URL, USER_DBA, PASSWORD_DBA);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Error al conectar a la base de datos: " + ex.getMessage());
+            return null;
+        }
+    }
+
+    // Conexion usuario FUNCIONARIO
+    public static Connection obtenerConexion_FUNCINARIO() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Cargar el controlador de MySQL
+            return DriverManager.getConnection(URL, USER_FUNCIONARIO, PASSWORD_FUNCIONARIO);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Error al conectar a la base de datos: " + ex.getMessage());
+            return null;
+        }
+    }
+
+
+    // Conexion usuario REPORTES
+    public static Connection obtenerConexion_REPORTES() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Cargar el controlador de MySQL
+            return DriverManager.getConnection(URL, USER_REPORTES, PASSWORD_REPORTES);
         } catch (ClassNotFoundException | SQLException ex) {
             System.out.println("Error al conectar a la base de datos: " + ex.getMessage());
             return null;
